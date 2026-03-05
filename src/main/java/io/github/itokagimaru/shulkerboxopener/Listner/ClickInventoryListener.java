c1ab73362a5b58a7b8211e8876a3ea344bd83071
sysnote8main <sysnote8main@gmail.com>
2026-03-05 21:07:20 +0900

    feat: more simple click handler

--- src/main/java/io/github/itokagimaru/shulkerboxopener/Listner/ClickInventoryListener.java
+++ src/main/java/io/github/itokagimaru/shulkerboxopener/Listner/ClickInventoryListener.java
@@ -2,6 +2,7 @@ package io.github.itokagimaru.shulkerboxopener.Listner;
 
 import io.github.itokagimaru.shulkerboxopener.gui.BaseGuiHolder;
 import io.github.itokagimaru.shulkerboxopener.gui.ShulkerOpenGUI;
+import java.util.Set;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Material;
@@ -12,35 +13,75 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 
 public class ClickInventoryListener implements Listener {
+    // supported inventory types
+    private static final Set<InventoryType> SUPPORTED_TYPES = Set.of(
+            InventoryType.PLAYER,
+            InventoryType.CRAFTING,
+            InventoryType.CHEST,
+            InventoryType.BARREL
+    );
+
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        Inventory inv = event.getInventory();
-        if (inv == null) return;
-        InventoryView view = event.getView();
-        InventoryType type = view.getTopInventory() == null
-                ? InventoryType.PLAYER
-                : view.getTopInventory().getType();
+        if(!(event.getWhoClicked() instanceof Player player)) return;
+        ItemStack clicked = event.getCurrentItem();
+
+        // check if top inventory is GUI
         if ((event.getView().getTopInventory().getHolder() instanceof BaseGuiHolder baseGuiHolder)) {
-            ItemStack clicked = event.getCurrentItem();
-            if (clicked == null || clicked.getType() == Material.AIR) return;
-            baseGuiHolder.onClick(event);
-        } else if (type == InventoryType.PLAYER || type == InventoryType.CRAFTING || type == InventoryType.CHEST || type == InventoryType.BARREL) {
-            ItemStack clicked = event.getCurrentItem();
-            Player player = (Player) event.getWhoClicked();
-            if (clicked == null || clicked.getType() == Material.AIR) return;
-            ItemStack clickedClone = clicked.clone();
-            if (!(event.isRightClick() && event.isShiftClick())) return;
-            if (clicked.getType() != Material.SHULKER_BOX) return;
-            event.setCancelled(true);
-            clicked.setAmount(0);
-            ShulkerOpenGUI shulkerOpenGUI = new ShulkerOpenGUI(inv, clickedClone);
-            if (shulkerOpenGUI.setup()) player.openInventory(shulkerOpenGUI.getInventory());
-            else player.sendMessage(Component.text("shulkerboxの読み込みに失敗しました").color(NamedTextColor.RED));
+            if (clicked != null && clicked.getType() != Material.AIR) {
+                baseGuiHolder.onClick(event);
+            }
+            return;
         }
+
+        // check if supported inventory to open shulker box
+        if(shouldOpenShulkerBoxGUI(event)) {
+            event.setCancelled(true);
+
+            // clone stack and remove original stack to prevent item duplication bug
+            ItemStack clickedClone = clicked.clone();
+            clicked.setAmount(0);
+
+            // open shulker box GUI
+            openShulkerBoxGUI(player, event.getInventory(), clickedClone);
+            return;
+        }
+    }
+
+    /**
+     * check if the player can open shulker box GUI
+     * @param event target inventory click event
+     * @return whether the player can open shulker box GUI or not
+     */
+    protected static boolean shouldOpenShulkerBoxGUI(InventoryClickEvent event) {
+        ItemStack clicked = event.getCurrentItem();
+        InventoryType type = event.getView().getTopInventory().getType();
+
+        return (event.isRightClick() && event.isShiftClick())
+                && clicked != null
+                && clicked.getType() == Material.SHULKER_BOX
+                && SUPPORTED_TYPES.contains(type);
+    }
+
+    /**
+     * open shulker box GUI
+     * @param player target player
+     * @param beforeInv current inventory
+     * @param shulkerBoxItem clicked shulker box item
+     * @return open success or not
+     */
+    protected static boolean openShulkerBoxGUI(Player player, Inventory beforeInv, ItemStack shulkerBoxItem) {
+        ShulkerOpenGUI shulkerOpenGUI = new ShulkerOpenGUI(beforeInv, shulkerBoxItem);
+        boolean setupResult = shulkerOpenGUI.setup();
+        if (setupResult) {
+            player.openInventory(shulkerOpenGUI.getInventory());
+        } else {
+            player.sendMessage(Component.text("シュルカーボックスの読み込みに失敗しました").color(NamedTextColor.RED));
+        }
+        return setupResult;
     }
 
     @EventHandler
@@ -51,6 +92,7 @@ public class ClickInventoryListener implements Listener {
         // get inventory and check
         Inventory inv = event.getInventory();
         if (!(inv.getHolder() instanceof BaseGuiHolder guiHolder)) return;
+
         // call onClose
         guiHolder.onClose(player);
     }
